64668380ce03c59470ee7a30d6621b43903281b2
Lipsamayee Jena <91116187+Lipsa760@users.noreply.github.com>
2023-09-26 09:56:18 +0530

    Update details.js

--- details.js
+++ details.js
@@ -1,30 +1,43 @@
+let ls = JSON.parse(localStorage.getItem("sdata"));
 const IMGPATH = "https://image.tmdb.org/t/p/w1280";
-const movieDetails = document.getElementById("container");
 
-const urlParams = new URLSearchParams(window.location.search);
-const movieId = urlParams.get("id");
-console.log(movieId);
-const apiUrl = `https://api.themoviedb.org/3/movie/${movieId}?api_key=e1aaad58747084b5874eded0abbe7c28`;
+let movieId = ls[0].id;
 
-detailMovies(apiUrl);
+const related_movie = `https://api.themoviedb.org/3/movie/${movieId}/similar?api_key=e1aaad58747084b5874eded0abbe7c28`;
 
-async function detailMovies(url) {
+let imgdiv = document.getElementById("image");
+let title = document.getElementById("title");
+let story = document.getElementById("story");
+let related_card = document.getElementById("box2");
+
+ls.forEach((ele) => {
+  let img = document.createElement("img");
+  img.setAttribute("src", `${IMGPATH + ele.poster_path}`);
+  imgdiv.append(img);
+  title.innerHTML = ele.title;
+  story.innerHTML = ele.overview;
+});
+
+getMovies(related_movie);
+async function getMovies(url) {
   const resp = await fetch(url);
   const respData = await resp.json();
+  console.log(respData);
   showMovies(respData.results);
 }
 function showMovies(movies) {
-  movieDetails.innerHTML = "";
-  const { poster_path, title, overview } = movie;
-  const movie_card = document.createElement("div");
-  movie_card.classList.add("movie");
+  related_card.innerHTML = "";
+  movies.forEach((movie) => {
+    const { poster_path, title, vote_average, overview } = movie;
+    const movie_card = document.createElement("div");
+    movie_card.classList.add("movie");
+    movie_card.innerHTML = `
+       <img src="${IMGPATH + poster_path}" alt="${title}"/>
 
-  movie_card.innerHTML = `
-         <img src="${IMGPATH + poster_path}" alt="${title}"/>
-  
-       <div class="movie-info">
-           <h3>${title}</h3>
-       </div> 
-       `;
-  root.appendChild(movie_card);
+     <div class="movie-info">
+         <h3>${title}</h3>
+     </div> 
+     `;
+    related_card.appendChild(movie_card);
+  });
 }
